14c3227fef005eeb6376175f714bd6edc59fb981
TuanOnta <mdz54dzakiarta@gmail.com>
2024-06-09 20:20:44 +0700

    style: delete unecessary comment

--- src/Albertus_FingerprintMatcher/imgMethod.cs
+++ src/Albertus_FingerprintMatcher/imgMethod.cs
@@ -47,8 +47,7 @@ namespace Albertus_FingerprintMatcher {
         {
             Bitmap resized = ResizeImage(filePath, 60, 60);
             byte[] bytes = BitmapToByteArray(resized);
-            string binaryString = Encoding.ASCII.GetString(bytes);
-            // karena 1 pixel gambar hitam putih membutuhkan 1 byte (8 bit) (alokasi memori) makanya ambil 30 string ditengah
+            string binaryString = Encoding.ASCII.GetString(bytes)
             binaryString = binaryString.Substring(binaryString.Length / 2 - 30, 60);
             return binaryString;
         }
